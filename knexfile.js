f4575610e3696b08faced9180d1ff7342752da37
Patrick <patrick.schwindt14@gmail.com>
2019-04-22 13:49:12 -0600

    Login and Register

---
+++ knexfile.js
@@ -0,0 +1,48 @@
+// Update with your config settings.
+localPgConnection = {
+    host: 'localhost',
+    database: 'users',
+    user: 'pat',
+    password: 'pass'
+  }
+  const prodDbConnection = process.env.DATABASE_URL || localPgConnection
+  
+  module.exports = {
+    development: {
+      client: 'sqlite3',
+      connection: {
+        filename: './data/auth.db3',
+      },
+      useNullAsDefault: true,
+      migrations: {
+        directory: './data/migrations',
+      },
+      seeds: {
+        directory: './data/seeds',
+      },
+    },
+    testing: {
+      client: 'sqlite3',
+      connection: {
+        filename: './data/test.db3',
+      },
+      useNullAsDefault: true,
+      migrations: {
+        directory: './data/migrations',
+      },
+      seeds: {
+        directory: './data/seeds',
+      },
+    },
+  
+    production:{
+      client: 'pg',
+      connection: prodDbConnection, //an object or string
+      migrations: {
+        directory: './data/migrations',
+      },
+      seeds: {
+        directory: './data/seeds',
+      },
+    }
+  };
