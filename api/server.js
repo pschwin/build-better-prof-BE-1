28265e55946e09ce0cf7b4b85337b837840a3dba
Patrick <patrick.schwindt14@gmail.com>
2019-04-24 16:45:10 -0600

    Login and Registration Working Again

--- api/server.js
+++ api/server.js
@@ -2,10 +2,15 @@ const express = require ('express');
 const server = express();
 const projectRouter = require('../data/routers/project-router.js');
 const studentRouter = require('../data/routers/student-router.js');
+const userRouter = require('../users/users-router');
+const authRouter = require('../auth/auth-router');
 
 server.use(express.json());
-server.use('/api/projects', projectRouter);
+server.use('/api/projects',  projectRouter);
 server.use('/api/students', studentRouter);
+server.use('/api/users', userRouter);
+server.use('/api/auth', authRouter);
+//server.use('/api/registration', userRouter);
 
 server.get('/', (req,res, next) => {
     res.send(`<h2>Server is Working</h2>`)
